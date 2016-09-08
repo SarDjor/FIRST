4009b99721f02d06cc46daf2bec7bb7d1651c49f
SarDjor <pascha-98@list.ru>
2016-09-08 23:27:09 +0300

    Фиксация из VisualStudio

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,41 @@
+// ConsoleApplication1.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include "Classes.h"
+using namespace std;
+
+
+Income operator+(Income &inc1, Income &inc2) {
+	Income rezult;
+	rezult.inc = inc1.inc + inc2.inc;
+	rezult.show();
+	return rezult;
+
+}
+
+int main()
+{
+	Income i1(15);
+	Income i2(30);
+	Income i3;
+	i3 = i1 + i2;
+	cout << i3;
+	Stuck stack;
+	int choice = 1;
+	setlocale(2, "");
+	while (choice) {
+		cout << "Choose:" << endl
+			<< "1 - Add customer" << endl
+			<< "2 - Delete customer" << endl
+			<< "3 - Show last one" << endl;
+		cin >> choice;
+		switch (choice) {
+		case 1: stack.push(); break;
+		case 2: stack.pop(); break;
+		case 3: stack.showfirst(); break;
+		default:break;
+		}
+	}
+}
